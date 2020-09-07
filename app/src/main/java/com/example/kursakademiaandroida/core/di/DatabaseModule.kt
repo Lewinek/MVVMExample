0a05c1b163933188de00d86b97cc0950f6ca7a74
Lewinek <cotek1993@wp.pl>
2020-09-07 07:42:41 +0200

    Added room and app module.

---
+++ app/src/main/java/com/example/kursakademiaandroida/core/di/DatabaseModule.kt
@@ -0,0 +1,13 @@
+package com.example.kursakademiaandroida.core.di
+
+import androidx.room.Room
+import com.example.kursakademiaandroida.core.database.AppDatabase
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val databaseModule = module {
+
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database")
+    }
+}
