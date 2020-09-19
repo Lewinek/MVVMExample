c95b29721e557e170ade56a9ccde03db30924aed
Lewinek <cotek1993@wp.pl>
2020-09-19 14:27:00 +0200

    Added EpisodeFragment.

---
+++ app/src/main/java/com/example/kursakademiaandroida/features/episodes/presentation/EpisodeFragment.kt
@@ -0,0 +1,36 @@
+package com.example.kursakademiaandroida.features.episodes.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.observe
+import com.example.kursakademiaandroida.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+class EpisodeFragment : Fragment() {
+
+    private val viewModel: EpisodeViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        observeEpisodes()
+    }
+
+    private fun observeEpisodes() {
+        viewModel.episodes.observe(this) {
+            //code to display episodes
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_episode, container, false)
+    }
+
+}
