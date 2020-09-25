d0228548fc9761ef868c987ba2eafd60084a9aef
Lewinek <cotek1993@wp.pl>
2020-09-25 14:58:56 +0200

    Added LocationModule.kt.

---
+++ app/src/main/java/com/example/kursakademiaandroida/features/location/di/LocationModule.kt
@@ -0,0 +1,21 @@
+package com.example.kursakademiaandroida.features.location.di
+
+import com.example.kursakademiaandroida.features.location.data.repository.LocationRepositoryImpl
+import com.example.kursakademiaandroida.features.location.domain.GetLocationsUseCase
+import com.example.kursakademiaandroida.features.location.domain.LocationRepository
+import com.example.kursakademiaandroida.features.location.presentation.LocationFragment
+import com.example.kursakademiaandroida.features.location.presentation.LocationViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+var locationModule = module {
+    //data
+    factory<LocationRepository> { LocationRepositoryImpl(get(), get(), get()) }
+
+    //domain
+    factory { GetLocationsUseCase(get()) }
+
+    //presentation
+    viewModel { LocationViewModel(get()) }
+    factory { LocationFragment() }
+}
