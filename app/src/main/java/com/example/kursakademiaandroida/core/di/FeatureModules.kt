a0ff8ae30b89b3438a16952131e89372512246bc
Lewinek <cotek1993@wp.pl>
2020-09-25 15:17:51 +0200

    Added locationModule to featureModules.

--- app/src/main/java/com/example/kursakademiaandroida/core/di/FeatureModules.kt
+++ app/src/main/java/com/example/kursakademiaandroida/core/di/FeatureModules.kt
@@ -1,9 +1,11 @@
 package com.example.kursakademiaandroida.core.di
 
 import com.example.kursakademiaandroida.features.episodes.di.episodeModule
+import com.example.kursakademiaandroida.features.location.di.locationModule
 import org.koin.core.module.Module
 
 
 var featureModules = listOf<Module>(
-    episodeModule
+    episodeModule,
+    locationModule
 )
